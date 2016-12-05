1eed5c6bb253dad36fe7f18816e15cd40cfc18e5
Derek Doyle <Derek.Doyle@D2L.com>
2016-12-05 16:15:14 -0500

    Added http-errors.  Corrected err to err.code.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var util = require('util');
+const httpErrors = require('http-errors');
 
 module.exports = function() {
   return function* (next) {
     try {
       yield* next;
     } catch (err) {
-      if (err === 'ConditionalCheckFailedException') {
-        throw new HttpError.NotFound(err.cause.message);
+      if (err.code === 'ConditionalCheckFailedException') {
+        throw new httpErrors.NotFound(err.cause.message);
       }
       throw err;
     }
